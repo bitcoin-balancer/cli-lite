24d451dc669b733133ff675bf738cd165bea4316
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2025-02-17 16:53:19 -0400

    Implement the getLandscapeSysinfo function
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,14 @@
 /* eslint-disable no-console */
 import process from 'node:process';
 import { printHeader } from './shared/print/index.js';
+import { getLandscapeSysinfo } from './shared/host/index.js';
 
 (async () => {
   try {
     // ...
     printHeader(
       '1.0.0',
-      `  System load:  2.73                Processes:                435
-  Usage of /:   11.8% of 456.34GB   Users logged in:          1
-  Memory usage: 50%                 IPv4 address for enp42s0: 192.168.1.14
-  Swap usage:   0%                  IPv4 address for tun0:    100.64.100.6
-  Temperature:  55.0 C
-
-  => There is 1 zombie process.`,
+      await getLandscapeSysinfo(),
       '',
     );
 
--- src/shared/host/index.ts
+++ src/shared/host/index.ts
@@ -3,8 +3,12 @@ import { execute } from '../command/index.js';
  *                                         IMPLEMENTATION                                         *
  ************************************************************************************************ */
 
-
+/**
+ * Executes the landscape-sysinfo command on the host machine and returns its results.
+ * @returns Promise<string>
+ */
 const getLandscapeSysinfo = async (): Promise<string> => execute('landscape-sysinfo', [], 'pipe');
+
 
 
 
