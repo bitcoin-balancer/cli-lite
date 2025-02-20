c51abed4c15f165fabee348fae15d04f953c6118
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2025-02-20 09:09:49 -0400

    Implement the down-up action - ready for testing
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

---
+++ src/actions/down-up.ts
@@ -0,0 +1,19 @@
+import { progressPrinterFactory } from '../modules/shared/print/index.js';
+import { HostService } from '../modules/host/index.js';
+
+/**
+ * down-up
+ * Stops containers and removes containers, networks, volumes, and images created by up. Afterwards,
+ * it pulls the latest images from the registry, creates and starts the containers
+ */
+export default async () => {
+  const progress = progressPrinterFactory('down-up', [
+    'Executing \'down\'...',
+    'Executing \'up\'...',
+  ]);
+  progress.step();
+  await HostService.down();
+  progress.step();
+  await HostService.up();
+  progress.step();
+};
